4ae52f1372ce068e271102192180f1c3b9165d98
GadgetZone <gadgetzone@nikihil-macbook-air.local>
2018-09-10 23:51:27 +0530

    Initial Commit

---
+++ sampleproj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleproj
+//
+//  Created by GadgetZone on 9/10/18.
+//  Copyright © 2018 GadgetZone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
